921b3ab0c2083490c9239b0277f2ea8428c76571
juanco1424 <78766144+juanco1424@users.noreply.github.com>
2022-08-31 20:31:10 -0500

    Se sube el progarma funcional y finalizado

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+
+function App() {
+  const[contar, setcontar] = useState(0)
+  const sumarUno = () =>{
+    setcontar(contar + 1)
+  }
+  return (
+    <div className="App">
+      <Cabecera stock = {contar}/>
+      <Listado sumarUno = {sumarUno}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+export default function Item({id, nombre, descripcion, stock, sumarUno}) {
+  const[cantidad, setCantidad] = useState(stock)
+
+  const setStock = () => {
+    setCantidad(cantidad - 1)
+    sumarUno();
+  }
+  
+  return (
+    <div className='producto' key ={id + nombre}>
+      {/* maquetar Item aquí */}
+      <h3>{nombre}</h3>
+      <p>{descripcion}</p>
+      <h5>Cantidad: <span>{(cantidad > 0 ? cantidad: "AGOTADO")}</span></h5>
+      <button disabled={cantidad === 0} onClick={() => {setStock();sumarUno()}}>{cantidad === 0 ? "AGOTADO":"COMPRAR"}</button>
+    </div>
+  )
+}
